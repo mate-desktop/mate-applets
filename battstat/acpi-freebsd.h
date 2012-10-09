0d9a417255df1620855f232717463097761b09d6
Steve Zesch <stevezesch2@gmail.com>
2012-10-09 11:52:46 -0400

    Fixed fsf addresses. Closes #18.

--- battstat/acpi-freebsd.h
+++ battstat/acpi-freebsd.h
@@ -13,7 +13,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02110-1301, USA.
  */
 
 #ifndef __ACPI_FREEBSD_H__
@@ -58,7 +58,7 @@ void acpi_freebsd_cleanup(struct acpi_info * acpiinfo);
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02110-1301, USA.
  */
 
 #ifndef __ACPI_FREEBSD_H__
