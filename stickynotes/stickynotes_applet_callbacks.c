a65f9460af5bf6a0172a59553aa9bd346b961b80
infirit <infirit@gmail.com>
2014-11-25 21:58:26 +0100

    stickynotes: we do not use the status anywhere so it has to go.

--- stickynotes/stickynotes_applet_callbacks.c
+++ stickynotes/stickynotes_applet_callbacks.c
@@ -183,13 +183,13 @@ void install_check_click_on_desktop (void)
 		gulong bytes;
 
 		/* We only use this extra property if the actual user-time property's missing */
-		int  status = XGetWindowProperty(GDK_DISPLAY_XDISPLAY(gdk_display_get_default()), desktop_window, user_time,
+		XGetWindowProperty(GDK_DISPLAY_XDISPLAY(gdk_display_get_default()), desktop_window, user_time,
 					0, 4, False, AnyPropertyType, &actual_type, &actual_format,
 					&nitems, &bytes, (unsigned char **)&data );
 		if (actual_type == None)
 		{
 			/* No user-time property, so look for the user-time-window */
-			status = XGetWindowProperty(GDK_DISPLAY_XDISPLAY(gdk_display_get_default()), desktop_window, user_time_window,
+			XGetWindowProperty(GDK_DISPLAY_XDISPLAY(gdk_display_get_default()), desktop_window, user_time_window,
 					0, 4, False, AnyPropertyType, &actual_type, &actual_format,
 					&nitems, &bytes, (unsigned char **)&data );
 			if (actual_type != None)
