2c985fd3b3a1690a5734bfc923a72bd4ee7381bd
Wolfgang Ulbrich <chat-to-me@raveit.de>
2015-12-17 16:03:47 +0100

    GTK3 trash: don't use deprecated gdk_error_trap_pop + GtkMisc

--- trashapplet/src/xstuff.c
+++ trashapplet/src/xstuff.c
@@ -250,8 +250,12 @@ xstuff_set_pos_size (GdkWindow *window, int x, int y, int w, int h)
 
 	gdk_window_move_resize (window, x, y, w, h);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+	gdk_error_trap_pop_ignored ();
+#else
 	gdk_flush ();
 	gdk_error_trap_pop ();
+#endif
 
 	g_object_set_data (G_OBJECT (window), "xstuff-cached-x", GINT_TO_POINTER (x));
 	g_object_set_data (G_OBJECT (window), "xstuff-cached-y", GINT_TO_POINTER (y));
