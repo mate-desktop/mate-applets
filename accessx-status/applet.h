cbc200ff01af3c7ba35475a7c8c83e127f893318
Victor Kareh <vkareh@vkareh.net>
2018-05-09 12:02:51 -0400

    accessx: Convert icons from pixbuf to cairo surfaces
    This improves support for HiDPI displays. It also fixes some deprecation
    warnings, adds auto-resizing, and updates the main applet icon.

--- accessx-status/applet.h
+++ accessx-status/applet.h
@@ -24,31 +24,56 @@
 
 #include <mate-panel-applet.h>
 
-#define ACCESSX_APPLET          "ax-applet"
-#define ACCESSX_BASE_ICON       "ax-base"
-#define ACCESSX_ACCEPT_BASE     "ax-accept"
-#define ACCESSX_REJECT_BASE     "ax-reject"
-#define MOUSEKEYS_BASE_ICON     "ax-mouse-base"
-#define MOUSEKEYS_BUTTON_LEFT   "ax-button-left"
-#define MOUSEKEYS_BUTTON_MIDDLE "ax-button-middle"
-#define MOUSEKEYS_BUTTON_RIGHT  "ax-button-right"
-#define MOUSEKEYS_DOT_LEFT      "ax-dot-left"
-#define MOUSEKEYS_DOT_MIDDLE    "ax-dot-middle"
-#define MOUSEKEYS_DOT_RIGHT     "ax-dot-right"
-#define SHIFT_KEY_ICON          "ax-shift-key"
-#define CONTROL_KEY_ICON        "ax-control-key"
-#define ALT_KEY_ICON            "ax-alt-key"
-#define META_KEY_ICON           "ax-meta-key"
-#define SUPER_KEY_ICON          "ax-super-key"
-#define HYPER_KEY_ICON          "ax-hyper-key"
-#define ALTGRAPH_KEY_ICON       "ax-altgraph-key"
-#define SLOWKEYS_IDLE_ICON      "ax-sk-idle"
-#define SLOWKEYS_PENDING_ICON   "ax-sk-pending"
-#define SLOWKEYS_ACCEPT_ICON    "ax-sk-accept"
-#define SLOWKEYS_REJECT_ICON    "ax-sk-reject"
-#define BOUNCEKEYS_ICON         "ax-bouncekeys"
+#define ACCESSX_APPLET            "preferences-desktop-accessibility"
 
-#define STATUS_APPLET_ICON_SIZE GTK_ICON_SIZE_LARGE_TOOLBAR
+#define ACCESSX_BASE_ICON         "mate-ax-key-none"
+#define ACCESSX_BASE_ICON_BASE    "mate-ax-key-base"
+#define ACCESSX_BASE_ICON_INVERSE "mate-ax-key-inverse"
+#define ACCESSX_ACCEPT_BASE       "mate-ax-key-yes"
+#define ACCESSX_REJECT_BASE       "mate-ax-key-no"
+
+#define MOUSEKEYS_BASE_ICON       "mate-mousekeys-base"
+#define MOUSEKEYS_BUTTON_LEFT     "mate-mousekeys-pressed-left"
+#define MOUSEKEYS_BUTTON_MIDDLE   "mate-mousekeys-pressed-middle"
+#define MOUSEKEYS_BUTTON_RIGHT    "mate-mousekeys-pressed-right"
+#define MOUSEKEYS_DOT_LEFT        "mate-mousekeys-default-left"
+#define MOUSEKEYS_DOT_MIDDLE      "mate-mousekeys-default-middle"
+#define MOUSEKEYS_DOT_RIGHT       "mate-mousekeys-default-right"
+
+#define SHIFT_KEY_ICON            "mate-sticky-shift-none"
+#define SHIFT_KEY_ICON_LATCHED    "mate-sticky-shift-latched"
+#define SHIFT_KEY_ICON_LOCKED     "mate-sticky-shift-locked"
+
+#define CONTROL_KEY_ICON          "mate-sticky-ctrl-none"
+#define CONTROL_KEY_ICON_LATCHED  "mate-sticky-ctrl-latched"
+#define CONTROL_KEY_ICON_LOCKED   "mate-sticky-ctrl-locked"
+
+#define ALT_KEY_ICON              "mate-sticky-alt-none"
+#define ALT_KEY_ICON_LATCHED      "mate-sticky-alt-latched"
+#define ALT_KEY_ICON_LOCKED       "mate-sticky-alt-locked"
+
+#define META_KEY_ICON             "mate-sticky-meta-none"
+#define META_KEY_ICON_LATCHED     "mate-sticky-meta-latched"
+#define META_KEY_ICON_LOCKED      "mate-sticky-meta-locked"
+
+#define HYPER_KEY_ICON            "mate-sticky-hyper-none"
+#define HYPER_KEY_ICON_LATCHED    "mate-sticky-hyper-latched"
+#define HYPER_KEY_ICON_LOCKED     "mate-sticky-hyper-locked"
+
+#define SUPER_KEY_ICON            "mate-sticky-super-none"
+#define SUPER_KEY_ICON_LATCHED    "mate-sticky-super-latched"
+#define SUPER_KEY_ICON_LOCKED     "mate-sticky-super-locked"
+
+#define ALTGRAPH_KEY_ICON         "mate-sticky-alt-none"
+#define ALTGRAPH_KEY_ICON_LATCHED "mate-sticky-alt-latched"
+#define ALTGRAPH_KEY_ICON_LOCKED  "mate-sticky-alt-locked"
+
+#define SLOWKEYS_IDLE_ICON        "mate-ax-slowkeys"
+#define SLOWKEYS_PENDING_ICON     "mate-ax-slowkeys-pending"
+#define SLOWKEYS_ACCEPT_ICON      "mate-ax-slowkeys-yes"
+#define SLOWKEYS_REJECT_ICON      "mate-ax-slowkeys-no"
+
+#define BOUNCEKEYS_ICON           "mate-ax-bouncekeys"
 
 typedef enum {
 	ACCESSX_STATUS_ERROR_NONE = 0,
@@ -73,7 +98,7 @@ typedef struct {
 	GtkWidget* alt_graph_indicator;
 	MatePanelAppletOrient orient;
 	GtkIconFactory* icon_factory;
-	gboolean initialized; 
+	gboolean initialized;
 	XkbDescRec* xkb;
 	Display* xkb_display;
 	AccessxStatusErrorType error_type;
