df235f442468ee91d7a00de8532c08c823024797
Laurent Napias <tamplan@free.fr>
2019-06-18 23:37:10 +0200

    [multiload] Add scroll interface tabs with mouse wheel

--- multiload/properties.c
+++ multiload/properties.c
@@ -36,6 +36,8 @@
 #define HIG_IDENTATION		"    "
 #define NEVER_SENSITIVE		"never_sensitive"
 
+static gboolean dialog_page_scroll_event_cb (GtkWidget *notebook, GdkEventScroll *event, GtkWindow *window);
+
 /* set sensitive and setup NEVER_SENSITIVE appropriately */
 static void
 hard_set_sensitive (GtkWidget *w, gboolean sensitivity)
@@ -639,6 +641,10 @@ fill_properties(GtkWidget *dialog, MultiloadApplet *ma)
 	add_color_selector (page, _("_Write"), "diskload-color1", ma);
 	add_color_selector (page, _("_Background"), "diskload-color2", ma);
 
+    gtk_widget_add_events (ma->notebook, GDK_SCROLL_MASK);
+    g_signal_connect (ma->notebook, "scroll-event",
+                G_CALLBACK (dialog_page_scroll_event_cb), control_vbox);
+
 	title = g_strconcat ("<span weight=\"bold\">", _("Network speed thresholds"), "</span>", NULL);
 	label = gtk_label_new (title);
 	gtk_label_set_use_markup (GTK_LABEL (label), TRUE);
@@ -832,4 +838,64 @@ multiload_properties_cb (GtkAction       *action,
 
 	gtk_notebook_set_current_page (GTK_NOTEBOOK (ma->notebook),
 				       ma->last_clicked);
+}
+
+static gboolean
+dialog_page_scroll_event_cb (GtkWidget *widget, GdkEventScroll *event, GtkWindow *window)
+{
+    GtkNotebook *notebook = GTK_NOTEBOOK (widget);
+    GtkWidget *child, *event_widget, *action_widget;
+
+    child = gtk_notebook_get_nth_page (notebook, gtk_notebook_get_current_page (notebook));
+    if (child == NULL)
+        return FALSE;
+
+    event_widget = gtk_get_event_widget ((GdkEvent *) event);
+
+    /* Ignore scroll events from the content of the page */
+    if (event_widget == NULL ||
+        event_widget == child ||
+        gtk_widget_is_ancestor (event_widget, child))
+        return FALSE;
+
+    /* And also from the action widgets */
+    action_widget = gtk_notebook_get_action_widget (notebook, GTK_PACK_START);
+    if (event_widget == action_widget ||
+       (action_widget != NULL && gtk_widget_is_ancestor (event_widget, action_widget)))
+        return FALSE;
+    action_widget = gtk_notebook_get_action_widget (notebook, GTK_PACK_END);
+    if (event_widget == action_widget ||
+       (action_widget != NULL && gtk_widget_is_ancestor (event_widget, action_widget)))
+        return FALSE;
+
+    switch (event->direction) {
+      case GDK_SCROLL_RIGHT:
+      case GDK_SCROLL_DOWN:
+	gtk_notebook_next_page (notebook);
+	break;
+      case GDK_SCROLL_LEFT:
+      case GDK_SCROLL_UP:
+	gtk_notebook_prev_page (notebook);
+	break;
+      case GDK_SCROLL_SMOOTH:
+	switch (gtk_notebook_get_tab_pos (notebook)) {
+	  case GTK_POS_LEFT:
+	  case GTK_POS_RIGHT:
+	    if (event->delta_y > 0)
+	      gtk_notebook_next_page (notebook);
+	    else if (event->delta_y < 0)
+	      gtk_notebook_prev_page (notebook);
+	    break;
+	  case GTK_POS_TOP:
+	  case GTK_POS_BOTTOM:
+	    if (event->delta_x > 0)
+	      gtk_notebook_next_page (notebook);
+	    else if (event->delta_x < 0)
+	      gtk_notebook_prev_page (notebook);
+	    break;
+	}
+	break;
+    }
+
+    return TRUE;
 }
